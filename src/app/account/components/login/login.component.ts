860a3993de41a185a4e90708f50b9ba56929133f
Amiran <todua.amiran97@gmail.com>
2022-01-27 23:49:41 +0200

    Implements Login component

--- src/app/account/components/login/login.component.ts
+++ src/app/account/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AccountFacadeService } from '../../services/account-facade.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  get username() { return this.loginForm?.get('username'); }
+  get password() { return this.loginForm?.get('password'); }
+
+  constructor(
+    private accountFacade: AccountFacadeService,
+    private router: Router) {
+      this.loginForm = new FormGroup({
+        username: new FormControl(null, [Validators.required]),
+        password: new FormControl(null, [Validators.required]),
+      });
+     }
 
   ngOnInit() {
+    
+  }
+
+  onSubmit() {
+    this.accountFacade.login(this.loginForm.value).subscribe(
+      user => {
+        this.router.navigate(['/products/list']);
+      },
+      error => {
+        console.error(error);
+      }
+    );
   }
 
 }
